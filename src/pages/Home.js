229e38f4f32eb61b065e2310d299c1b8eeaad88a
Adilehkhub <adilehk@gmail.com>
2025-07-25 15:10:19 -0700

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section className="p-8">
+      <h1 className="text-4xl font-bold mb-4">Welcome to StratScale</h1>
+      <p>Helping you scale programs with precision and execution excellence.</p>
+    </section>
+  );
+}
+
+export default Home;
